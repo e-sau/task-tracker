eadc8eef76d31a10b7490ee8fe0b42debe150bdc
ToSPP <esau@yandex.ru>
2020-01-12 23:36:55 +0300

    Lesson 2

---
+++ frontend/models/Task.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace frontend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "task".
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $description
+ * @property int $status
+ * @property int $priority
+ * @property int|null $created_at
+ * @property int|null $updated_at
+ */
+class Task extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'task';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'description', 'status', 'priority'], 'required'],
+            [['status', 'priority', 'created_at', 'updated_at'], 'integer'],
+            [['title', 'description'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'description' => 'Description',
+            'status' => 'Status',
+            'priority' => 'Priority',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+}
