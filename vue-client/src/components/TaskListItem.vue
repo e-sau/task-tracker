8fc12715a48024a10cd0063784258e54b1972a9e
ToSPP <esau@yandex.ru>
2020-01-30 21:41:58 +0300

    Lesson 6: Добавил клиента для Yii2 API на Vue.js

---
+++ vue-client/src/components/TaskListItem.vue
@@ -0,0 +1,29 @@
+<template>
+    <tr>
+        <td>{{ task.id }}</td>
+        <td>{{ task.title }}</td>
+        <td>{{ task.description }}</td>
+    </tr>
+</template>
+
+<script>
+  export default {
+    props: {
+      task: {
+        type: Object,
+        required: true
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    td {
+        padding: 5px 7px;
+        text-align: center;
+        border: 1px solid #ccc;
+    }
+    td:nth-child(3) {
+        text-align: left;
+    }
+</style>
